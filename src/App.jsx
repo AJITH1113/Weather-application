ddf02d1fa5a6092a35caef4982ea6b49b0a02dd6
AJITH1113 <ajiajith1113@gmail.com>
2024-12-12 08:56:20 +0530

    weather application

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Weather from "./components/Weather";
+const App = () => {
+  return (
+    <div className="app">
+      <Weather />
+      {/* 6eaa9f8f3533ca628b2579f85e74e0d1 */}
+    </div>
+  );
+};
+
+export default App;
